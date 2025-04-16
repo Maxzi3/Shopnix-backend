677b5c0f24d592a695119b71697fc8976467b649
Maxzi3 <obisikemaxwell@gmail.com>
2025-04-16 18:55:49 +0100

    worked on the user section but omo

--- models/userModel.js
+++ models/userModel.js
@@ -3,66 +3,71 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: [true, "Please tell us your fullName!"],
-  },
-  email: {
-    type: String,
-    required: [true, "Please provide your email"],
-    unique: true,
-    lowercase: true,
-    validate: [validator.isEmail, "Please enter a valid email"],
-  },
-  phoneNumber: {
-    type: String,
-    required: [true, "Please provide your phone number"],
-  },
-  address: {
-    type: String,
-  },
-  photo: {
-    type: String,
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-  },
-  emailVerified: {
-    type: String,
-    enum: ["pending", "verified"],
-    default: "pending",
-  },
-  password: {
-    type: String,
-    required: [true, "Please provide a password"],
-    minlength: 8,
-    select: false,
-  },
-  passwordConfirm: {
-    type: String,
-    required: [true, "Please confirm your password"],
-    validate: {
-      // This only works on Create and Save
-      validator: function (el) {
-        return el === this.password;
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, "Please tell us your fullName!"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide your email"],
+      unique: true,
+      lowercase: true,
+      validate: [validator.isEmail, "Please enter a valid email"],
+    },
+    phoneNumber: {
+      type: String,
+      required: [true, "Please provide your phone number"],
+    },
+    address: {
+      type: String,
+    },
+    photo: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    emailVerified: {
+      type: String,
+      enum: ["pending", "verified"],
+      default: "pending",
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: 8,
+      select: false,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, "Please confirm your password"],
+      validate: {
+        // This only works on Create and Save
+        validator: function (el) {
+          return el === this.password;
+        },
+        message: "Passwords do not match. Please re-enter.",
       },
-      message: "Passwords do not match. Please re-enter.",
+    },
+    passwordChangedAt: {
+      type: Date,
+    },
+    passwordResetToken: String,
+    passwordResetExpiresAt: Date,
+    active: {
+      type: Boolean,
+      default: true,
+      select: false,
     },
   },
-  passwordChangedAt: {
-    type: Date,
-  },
-  passwordResetToken: { String, select: false },
-  passwordResetExpiresAt: { Date, select: false },
-  active: {
-    type: Boolean,
-    default: true,
-    select: false,
-  },
-});
+  {
+    timestamps: true, // 
+  }
+);
 userSchema.pre("save", async function (next) {
   // Only run this function if password was modified
   if (!this.isModified("password")) return next();
